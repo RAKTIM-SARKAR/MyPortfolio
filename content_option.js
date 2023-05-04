cd99ced0ecace135f850deeb444fbf48467b129d
Raktim Sarkar <61552807+RAKTIM-SARKAR@users.noreply.github.com>
2023-05-04 07:26:13 +0530

    Add files via upload

---
+++ content_option.js
@@ -0,0 +1,167 @@
+const logotext = "RAKTIM";
+const meta = {
+    title: "Raktim Sarkar",
+    description: "I’m Raktim Sarkar Web_developer _ React.js devloper,currently studied Masters in Computer Application in Assam Don Bosco University, Azara Guwahati",
+};
+
+const introdata = {
+    title: "I’m RAKTIM SARKAR",
+    animated: {
+        first: "I love coding",
+        second: "I code Dynamic web Apps",
+        third: "I develop Android apps",
+    },
+    description: "I am a computer science student with a passion for technology and problem-solving. I am currently pursuing MASTERS in COMPUTER APPLICATION in ASSAM DON BOSCO UNIVERSITY, AZARA GUWAHATI.",
+    your_img_url: "https://i.pinimg.com/564x/21/3a/61/213a61dc8c308ef4f22779d72a214ee2.jpg",
+   
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "I’m Raktim Sarkar Web_developer _ React.js devloper,currently studied Masters in Computer Application in Assam Don Bosco University, Azara Guwahati.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "HTML/CSS",
+        value: 90,
+    },
+    {
+        name: "Python",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "PL/SQL",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "I have designed lot of UI.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "I have done my MAJOR PROJECT on ANDROID DEVELOPMENT, all the UI/UX and the Database part of this project was done by me",
+    },
+    {
+        title: "Rechers & Development",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "An Android application for Home Essensial service.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "name@raktimsarkar656@gmail.com",
+    YOUR_FONE: "9394222372",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/raktim-png",
+    facebook: "https://facebook.com/rocktim.kashyap.71",
+    linkedin: "https://linkedin.com/in/raktim-sarkar",
+    twitter: "https://twitter.com/Raktim731653611",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
